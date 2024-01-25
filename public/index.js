844fa853f201777c772e5f7d3ca0f714d051b0d6
Vanessa Kasminoff <sunnset07@gmail.com>
2024-01-24 21:59:38 -0800

    Created an array with the mockData and imported Chartjs cdn

--- public/index.js
+++ public/index.js
@@ -1,9 +1,41 @@
+import mockData from "./mockData.js";
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=6d9c3e72f4d04956b9de53b24e32783b');
+
+    // let resultData = await response.json();
+
+    // console.log(resultData);
+
+    // let stocks = Object.entries(resultData);
+
+    // console.log(stocks);
+
+    const {GME, MSFT, DIS, BNTX} = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+    console.log(stocks)
+
+    let myChart = new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)'
+                ],
+            }]
+        }
+    });
 }
 
 main()
