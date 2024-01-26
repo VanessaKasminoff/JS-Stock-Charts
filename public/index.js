d912d1d015d48a39d935df070244ccb18b6011db
Vanessa Kasminoff <sunnset07@gmail.com>
2024-01-26 02:03:43 -0800

    Created the average price chart and added a function to calculate the average. Changed the color of MSFT stock

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ function stockColor(stock) {
         return '#925E78'
     }
     if (stock === 'MSFT') {
-        return '#BD93BD'
+        return '#251351'
     }
     if (stock === 'DIS') {
         return '#F05365'
@@ -16,7 +16,7 @@ function stockColor(stock) {
     }
 }
 
-//
+// compares each high value against 0 and then sets the max to that value to obtain the highest value.
 function getHighestValue(stockData) {
   let highestValues = {};
   
@@ -39,25 +39,36 @@ function getHighestValue(stockData) {
 
 }
 
+// calculates average stock values
+function getAverageValues(values) {
+  let total = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    total += parseFloat(values[i].high)
+  };
+
+  return (total / values.length).toFixed(2);
+}
+
 async function main() {
   const timeChartCanvas = document.querySelector("#time-chart");
   const highestPriceChartCanvas = document.querySelector("#highest-price-chart");
   const averagePriceChartCanvas = document.querySelector("#average-price-chart");
 
-  // let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=6d9c3e72f4d04956b9de53b24e32783b');
+/*
+  Using the mockData instead of the API because of testing purposes.
+  However, the resultData fetched from the API works as intended if used instead.
+*/
 
+  // let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=15min&apikey=6d9c3e72f4d04956b9de53b24e32783b');
   // let resultData = await response.json();
-
   // console.log(resultData);
-
   // const {GME, MSFT, DIS, BNTX} = resultData;
-  // const stocks = [GME, MSFT, DIS, BTNX];
-
+  // const stocks = [GME, MSFT, DIS, BNTX];
   // console.log(stocks);
 
   const { GME, MSFT, DIS, BNTX } = mockData;
   const stocks = [GME, MSFT, DIS, BNTX];
-  console.log(stocks);
 
   stocks.forEach(stock => stock.values.reverse());
 
@@ -90,18 +101,18 @@ async function main() {
   });
 
   // AVERAGE STOCK PRICE - CHART
-  // new Chart(averagePriceChartCanvas.getContext("2d"), {
-  //   type: "pie",
-  //   data: {
-  //     labels: stocks.map(stock => stock.meta.symbol),
-  //     datasets: [{
-  //       label: 'Highest',
-  //       data: getHighestValue(stocks),
-  //       backgroundColor: stocks.map(stock => (stockColor(stock.meta.symbol))),
-  //       borderColor: stocks.map(stock => (stockColor(stock.meta.symbol))),
-  //     }]
-  //   },
-  // });
+  new Chart(averagePriceChartCanvas.getContext("2d"), {
+    type: "pie",
+    data: {
+      labels: stocks.map(stock => stock.meta.symbol),
+      datasets: [{
+        label: 'Average',
+        data: stocks.map(stock => (getAverageValues(stock.values))),
+        backgroundColor: stocks.map(stock => (stockColor(stock.meta.symbol))),
+        borderColor: stocks.map(stock => (stockColor(stock.meta.symbol))),
+      }]
+    },
+  });
 }
 
 main();
